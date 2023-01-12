1fcdb0bf9f73779da2cd23dfe3c3fe51c5aea6c2
Paziu <113362954+Paziu050@users.noreply.github.com>
2023-01-12 21:27:02 +0100

    Add files via upload

---
+++ 09.01.js
@@ -0,0 +1,8 @@
+const  = bbbb document.querySelector("header");
+
+bbbb.style.backgroundColor = "yellow";
+
+const aaaa = document.querySelector("header h1");
+console.log(aaaa);
+aaaa.style.fontFamily = "Arial Black";
+aaaa.innerHTML = "preambuła";
