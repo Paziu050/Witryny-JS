38cd4739dfa00b7b62a8f78bbef2b83a35bf0a9e
Paziu <113362954+Paziu050@users.noreply.github.com>
2023-03-05 20:35:48 +0100

    Create 03.03.js

---
+++ 03.03.js
@@ -0,0 +1,71 @@
+document.querySelector("#pierwszy").addEventListener("click", function (){
+    const przyc = document.querySelector("#cosik");
+    console.log(przyc.dataset.x)
+    
+    if(cosik.style.display == 'none')
+    {
+        cosik.style.display = 'block';
+    }
+    else
+    {
+        cosik.style.display = 'none';
+    }
+})
+//
+document.querySelector("#drugi").addEventListener("click", function (){
+    const przyci = document.querySelector("#cosik2");
+    console.log(przyci.dataset.y)
+    
+    if(cosik2.style.display == 'none')
+    {
+        cosik2.style.display = 'block';
+    }
+    else
+    {
+        cosik2.style.display = 'none';
+    }
+})
+//
+document.querySelector("#trzeci").addEventListener("click", function (){
+    const przycis = document.querySelector("#cosik3");
+    console.log(przycis.dataset.z)
+    
+    if(cosik3.style.display == 'none')
+    {
+        cosik3.style.display = 'block';
+    }
+    else
+    {
+        cosik3.style.display = 'none';
+    }
+})
+//
+document.querySelector("#czwarty").addEventListener("click", function (){
+    const przycisk = document.querySelector("#cosik4");
+    console.log(przycisk.dataset.c)
+    
+    if(cosik4.style.display == 'none')
+    {
+        cosik4.style.display = 'block';
+    }
+    else
+    {
+        cosik4.style.display = 'none';
+    }
+})
+//
+document.querySelector("#piaty").addEventListener("click", function (){
+    const przycisk1 = document.querySelector("#cosik5");
+    console.log(przycisk1.dataset.v)
+    
+    if(cosik5.style.display == 'none')
+    {
+        cosik5.style.display = 'block';
+    }
+    else
+    {
+        cosik5.style.display = 'none';
+    }
+    document.querySelector("#piaty").addEventListener("click")
+})
+
