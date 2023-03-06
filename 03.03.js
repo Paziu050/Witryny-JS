7410710c003341ed31afea903e93028a92b37441
Paziu <113362954+Paziu050@users.noreply.github.com>
2023-03-06 17:12:42 +0100

    Update 03.03.js

--- 03.03.js
+++ 03.03.js
@@ -1,71 +1,95 @@
 document.querySelector("#pierwszy").addEventListener("click", function (){
     const przyc = document.querySelector("#cosik");
+    const elo = document.querySelector("#pierwszy");
     console.log(przyc.dataset.x)
-    
-    if(cosik.style.display == 'none')
+    if(cosik.style.display == 'block')
     {
-        cosik.style.display = 'block';
+        cosik.style.display = 'none';
+        elo.innerHTML="&#8615;";
     }
     else
     {
-        cosik.style.display = 'none';
+       
+        cosik.style.display = 'block';
+        elo.innerHTML="&#8613;";
+        
     }
 })
 //
 document.querySelector("#drugi").addEventListener("click", function (){
     const przyci = document.querySelector("#cosik2");
+    const ello = document.querySelector("#drugi");
     console.log(przyci.dataset.y)
     
-    if(cosik2.style.display == 'none')
+    if(cosik2.style.display == 'block')
     {
-        cosik2.style.display = 'block';
+        cosik2.style.display = 'none';
+        ello.innerHTML="&#8615;";
     }
     else
     {
-        cosik2.style.display = 'none';
+        cosik2.style.display = 'block';
+        ello.innerHTML="&#8613;";
+
     }
 })
 //
 document.querySelector("#trzeci").addEventListener("click", function (){
     const przycis = document.querySelector("#cosik3");
+    const elllo = document.querySelector("#trzeci");
     console.log(przycis.dataset.z)
     
-    if(cosik3.style.display == 'none')
+    if(cosik3.style.display == 'block')
     {
-        cosik3.style.display = 'block';
+        cosik3.style.display = 'none';
+        elllo.innerHTML="&#8615;";
     }
     else
     {
-        cosik3.style.display = 'none';
+        cosik3.style.display = 'block';
+        elllo.innerHTML="&#8613;";
     }
 })
 //
 document.querySelector("#czwarty").addEventListener("click", function (){
     const przycisk = document.querySelector("#cosik4");
+    const ellllo = document.querySelector("#czwarty");
     console.log(przycisk.dataset.c)
     
-    if(cosik4.style.display == 'none')
+    if(cosik4.style.display == 'block')
     {
-        cosik4.style.display = 'block';
+        cosik4.style.display = 'none';
+        ellllo.innerHTML="&#8615;";
     }
     else
     {
-        cosik4.style.display = 'none';
+        cosik4.style.display = 'block';
+        ellllo.innerHTML="&#8613;";
     }
 })
 //
 document.querySelector("#piaty").addEventListener("click", function (){
     const przycisk1 = document.querySelector("#cosik5");
+    const elllllo = document.querySelector("#piaty");
     console.log(przycisk1.dataset.v)
     
-    if(cosik5.style.display == 'none')
+    if(cosik5.style.display == 'block')
     {
-        cosik5.style.display = 'block';
+        cosik5.style.display = 'none';
+        elllllo.innerHTML="&#8615;";
     }
     else
     {
-        cosik5.style.display = 'none';
+        cosik5.style.display = 'block';
+        elllllo.innerHTML="&#8613;";
     }
-    document.querySelector("#piaty").addEventListener("click")
 })
+
+
+document.querySelectorAll(taka).addEventListener("click", function (){
+    console.log(taka)
+    const mmm = document.getElementsByClassName(a)
+})
+
+
 
